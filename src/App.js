81db3a0b71d73640193f90e1bc1ade745da5231c
Theo Cho <cdsleaf@gmail.com>
2020-01-05 23:55:17 +1300

    chore: add typescript package & update apollo package

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { HashRouter as Router, Route } from "react-router-dom";
-import { ApolloProvider } from "react-apollo";
+import { ApolloProvider } from "@apollo/react-hooks";
 import client from "./apolloClient";
 import Home from "./Home";
 import Detail from "./Detail";
