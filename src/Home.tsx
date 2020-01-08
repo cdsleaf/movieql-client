a1dc970de4d05d09bef6bb3775c511938cc44b4d
Theo Cho <cdsleaf@gmail.com>
2020-01-08 23:51:04 +1300

    refactor: convert Home component to Typescript

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,6 +4,18 @@ import { useQuery } from '@apollo/react-hooks';
 import { HOME_PAGE } from "./queries";
 import Movie from "./Movie";
 
+interface MovieType {
+  id: number;
+  title: string;
+  genres: string;
+  rating: number;
+  medium_cover_image: string;
+}
+
+interface MoviesType {
+  movies: MovieType[];
+}
+
 const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 0.7fr);
@@ -11,14 +23,14 @@ const Container = styled.div`
 `;
 
 function Home() {
-  const { loading, error, data} = useQuery(HOME_PAGE);
+  const { loading, error, data} = useQuery<MoviesType>(HOME_PAGE);
 
   if(loading) return <p>Loading...</p>;
   if(error) return <p>Error</p>;
   
   return (
     <Container>
-      {data.movies.map(movie => (
+      {data && data.movies.map(movie => (
         <Movie 
           id={movie.id}
           key={movie.id}
